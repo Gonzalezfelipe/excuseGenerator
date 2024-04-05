de64a43db6c6dba69b053e676ffddb96d7653038
Felipe González <160081406+Gonzalezfelipe@users.noreply.github.com>
2024-04-05 22:41:08 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse")
+  excuse.textContent = randomSentence();
 };
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+const randomSentence = () => {
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  return (`${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`)
+  
+}
+
+
+
+
