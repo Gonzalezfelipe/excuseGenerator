477e21645d00d50746ac856c186034a7bc4df237
Felipe González <160081406+Gonzalezfelipe@users.noreply.github.com>
2024-05-06 13:24:52 +0000

    Se incluyen variables dentro de la función

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ window.onload = function() {
   excuse.textContent = randomExcuseSentence();
 };
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
 
 const randomExcuseSentence = () => {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
   let whoRandomIndex = who[Math.floor(Math.random() * who.length)];
   let actionRandomIndex = action[Math.floor(Math.random() * action.length)];
   let whatRandomIndex = what[Math.floor(Math.random() * what.length)];
